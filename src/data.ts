80df970ae15e97451079dc641f7f110343d5adbe
yenfangchien <93360756+Yen-Fang@users.noreply.github.com>
2026-04-25 12:54:27 +0800

    Remove unused memory imports from data.ts

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,8 @@
 import { MarathonMemory } from "./types";
 import { memory1 } from "./memories/memory1";
-import { memory2 } from "./memories/memory2";
-import { memory3 } from "./memories/memory3";
+
 
 export const INITIAL_MEMORIES: MarathonMemory[] = [
-  memory1,
-  memory2,
-  memory3
+  memory1
 ];
 
