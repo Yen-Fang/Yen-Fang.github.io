7f65ec9d6d9e7166c8ac5cff0b4b328a2d1a4fe3
yenfangchien <93360756+Yen-Fang@users.noreply.github.com>
2026-04-27 12:08:00 +0800

    Rename imported memory for consistency

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,8 @@
 import { MarathonMemory } from "./types";
-import { 260201-ehime } from "./memories/260201-ehime";
+import { ehime260201 } from "./memories/260201-ehime";
 
 
 export const INITIAL_MEMORIES: MarathonMemory[] = [
-  260201-ehime
+  ehime260201
 ];
 
