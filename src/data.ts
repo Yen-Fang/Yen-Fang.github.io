0572affeba818637542de064a7c9e1841d8085f9
yenfangchien <93360756+Yen-Fang@users.noreply.github.com>
2026-04-27 16:58:17 +0800

    Update import for taipei251220 memory file

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 import { MarathonMemory } from "./types";
-import { taipei251220 } from "./memories/251214-taipei";
+import { taipei251220 } from "./memories/251221-taipei";
 import { ehime260201 } from "./memories/260201-ehime";
 
 
