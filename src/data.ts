7a7aaa50e0484b880e0b0ecf2bf2404b2eb4e60f
yenfangchien <93360756+Yen-Fang@users.noreply.github.com>
2026-04-27 16:51:27 +0800

    Add taipei251220 to INITIAL_MEMORIES

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,10 @@
 import { MarathonMemory } from "./types";
+import { taipei251220 } from "./memories/251214-taipei";
 import { ehime260201 } from "./memories/260201-ehime";
 
 
 export const INITIAL_MEMORIES: MarathonMemory[] = [
+  taipei251220,
   ehime260201
 ];
 
